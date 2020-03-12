a13474483497f8447499b2335be5faf01558df61
Guilliam Lutz <guilliamlutz@outlook.com>
2020-03-12 11:59:34 +0100

    Startmenu added

--- src/Launcher.java
+++ src/Launcher.java
@@ -1,6 +1,6 @@
-public class Launcher {
+/*public class Launcher {
 
     public static void main(String[] args) {
         new Main("Dungeon",1920,1080 );
     }
-}
+}*/
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,13 @@
-import javafx.animation.AnimationTimer;
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.layout.BorderPane;
+import javafx.scene.control.Button;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 import org.jfree.fx.FXGraphics2D;
 import org.jfree.fx.ResizableCanvas;
+
 
 import java.awt.*;
 import java.awt.geom.AffineTransform;
@@ -12,29 +15,69 @@ import java.awt.geom.AffineTransform;
 public class Main extends Application {
     private ResizableCanvas canvas;
     private Map map;
-    private Display.Scene scene;
 
     public int width;
     public int height;
 
-    public Main(String title, int width, int height) {
+    /*public Main(String title, int width, int height) {
         this.width = width;
         this.height = height;
         this.scene = new Display.Scene(title, width, height);
+    }*/
+
+    public static void main(String[] args) {
+        launch(Main.class);
     }
 
     @Override
     public void start(Stage stage) throws Exception {
+        Button start = new Button("Start Game");
+        start.setFont(Font.font("Kalam", 50));
+        start.setTextFill(javafx.scene.paint.Paint.valueOf("White"));
+        start.setStyle("-fx-border-color: transparent;" +
+                " -fx-border-width: 0;\n" +
+                "    -fx-background-radius: 0;\n" +
+                "    -fx-background-color: transparent;"+
+                "-fx-stroke: firebrick;"+
+        "-fx-stroke-width: 2px;"
+                );
 
-        BorderPane mainPane = new BorderPane();
-        canvas = new ResizableCanvas(g -> draw(g), mainPane);
-        mainPane.setCenter(canvas);
+
+        BorderPane menuPane = new BorderPane();
+
+        Image image2 = new Image(getClass().getResourceAsStream("/dungeonAchtergrond.jpg"));
+
+        BackgroundSize bSize = new BackgroundSize(1920, 1100, false, false, true, true);
+
+        Background background2 = new Background(new BackgroundImage(image2,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER,
+                bSize));
+
+        menuPane.setBackground(background2);
+
+        menuPane.setPrefHeight(1080);
+        menuPane.setMaxHeight(1080);
+        menuPane.setPrefWidth(1920);
+        menuPane.setMaxWidth(1920);
+        menuPane.setCenter(start);
+
+
+        stage.setScene(new Scene(menuPane));
+
+        BorderPane gamePane = new BorderPane();
+
+        canvas = new ResizableCanvas(g -> draw(g), gamePane);
+
+
+        gamePane.setCenter(canvas);
         FXGraphics2D g2d = new FXGraphics2D(canvas.getGraphicsContext2D());
 
         map = new Map("Dungeon2.json");
 
 
-        stage.setScene(new Scene(mainPane));
+        start.setOnAction(event -> stage.setScene(new Scene(gamePane)));
         stage.setTitle("Game");
         stage.show();
         draw(g2d);
